1d0693f5717fd61d818c44f34912d13ce42d1ce5
Charley Cowens <charley.cowens@gmail.com>
2016-08-07 23:43:20 -0700

    plots and tables

--- read-in-data.R
+++ read-in-data.R
@@ -27,18 +27,24 @@ by_election <- all_data %>%
   summarise(Open.Seats = sum(Won), 
             Candidates = n(),
             Competition = Candidates/Open.Seats,
-            Voting.On.Race = min(Total.Votes.Cast)/Open.Seats,
+            Voting.On.Race = as.integer(min(Total.Votes.Cast)/Open.Seats),
             Incumbent.Running.Rate = sum(Incumbent)/Open.Seats,
             Incumbent.Success.Rate = sum(Incumbent & Won) / sum(Incumbent),
             New.Members = sum(!Incumbent & Won)
             )
-ggplot(data = by_election) + 
+
+ggplot(data = by_election) +
+  ggtitle("Total Voting on School Board Race") +
   theme_minimal() +
   aes(x = Year, y = Voting.On.Race) + 
-  geom_text(aes(label=Year)) +
-  theme(axis.title.x=element_blank(),
-                    axis.text.x=element_blank(),
-                    axis.ticks.x=element_blank())
+  geom_text(aes(label=Year, color="red")) +
+  theme(axis.title.y=element_blank(),
+        axis.title.x=element_blank(),
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank(),
+          panel.grid.major.x = element_blank(),
+          legend.position="none") +
+  scale_y_continuous(breaks = seq(10000,60000,10000), limits=c(20000,60000))
 
 by_candidate <- all_data %>%
   group_by(PersonID) %>%
@@ -56,11 +62,24 @@ by_incumbent <- filter(all_data, Incumbent) %>%
   summarise(Times.Ran = n(),
             Times.Won = sum(Won),
             Times.Lost = Times.Ran-Times.Won,
-            Success.Rate = percent(Times.Won/Times.Ran, digits = 0)
+            Success.Rate = percent(Times.Won/Times.Ran, digits = 1)
   ) %>%
   arrange(desc(Success.Rate), desc(Times.Won), desc(Times.Lost))
 
-formattable(by_incumbent, list(Times.Won = color_bar("lightgray", proportion)), caption="WCCUSD Incumbent Candidates 1995- Sorted by Success then Victories")
+last_row <- summarise(by_incumbent, n(), sum(Times.Ran), sum(Times.Won), sum(Times.Lost), percent(sum(Times.Won)/sum(Times.Ran)))
+colnames(last_row) <- colnames(by_incumbent)
+last_row_num <- as.integer(last_row[, 1]) + 1 
+last_row[, 1] <- ""
+by_incumbent <- rbind(by_incumbent,last_row)
+
+
+formattable(by_incumbent, list(  Times.Won = color_bar("lightgray", proportion),
+                                 area(row = last_row_num) ~ formatter("span", style = "font-weight: bold"),
+                                 area(row = last_row_num, col = 1:4) ~ formatter("span", style = "font-style: italic"),
+                                 area(row = 1:(last_row_num-1), col = 5) ~ formatter("span", style = "color: white")
+), 
+            caption="WCCUSD Incumbent Candidates 1995 till Now Sorted by Success then Victories")
+
 
 #look at ballot designations
 
@@ -105,4 +124,21 @@ term_success <- transmute(term_success, Term,
 term_success$Success.Rate <- percent(term_success$Success.Rate)
 
 
+#ballot position
 
+position_data <- filter(all_data, !is.na(Ballot.Position))
+position_data <- mutate(position_data,
+                  Ballot.Position.Insideness = pmin(Ballot.Position-1, Number.of.Candidates-Ballot.Position)) %>%
+                  select(Ballot.Position, Ballot.Position.Insideness, Won)
+
+ggplot(summarise(group_by(position_data,Ballot.Position.Insideness), Success.Rate = sum(Won)/n())) +
+  ggtitle("Likelihood of Winning (0..1) for Each Ballot Position by Distance from First or Last") +
+  aes(x =  Ballot.Position.Insideness, y = Success.Rate) + 
+  theme_minimal() + 
+  geom_point()
+
+ggplot(summarise(group_by(position_data,Ballot.Position), Success.Rate = sum(Won)/n())) +
+  ggtitle("Likelihood of Winning (0..1) for Each Position in Listing on Ballot") +
+  aes(x =  Ballot.Position, y = Success.Rate) + 
+  theme_minimal() +
+  geom_point()
